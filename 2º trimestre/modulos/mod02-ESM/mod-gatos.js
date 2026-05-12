82be732af5b38673f2d89118691b1d3d4c90be45
ThiagoKrudes <thiago.krudes.oliveira@escola.pr.gov.br>
2026-05-12 13:41:33 +0000

    organizar pastas do primeiro e segundo trimestres e pastas de modulos

---
+++ 2º trimestre/modulos/mod02-ESM/mod-gatos.js
@@ -0,0 +1,13 @@
+let racaViraLata = 'Vira-Lata'
+
+export function banhoGato(racaGato, tipoBanho){
+
+}
+
+export function tosaGato(racaGato, tipoTosa){
+    
+}
+
+export function alimentarGato(racaGato, tipoRacao){
+    
+}
