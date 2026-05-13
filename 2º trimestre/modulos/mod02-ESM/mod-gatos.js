d1da1212f6e16c37add2ecc289ef3b6c7dcc3e2e
ThiagoKrudes <thiago.krudes.oliveira@escola.pr.gov.br>
2026-05-13 11:44:06 +0000

    finalizacao ESM modulo Gatos

--- 2º trimestre/modulos/mod02-ESM/mod-gatos.js
+++ 2º trimestre/modulos/mod02-ESM/mod-gatos.js
@@ -1,13 +1,13 @@
-let racaViraLata = 'Vira-Lata'
+
 
 export function banhoGato(racaGato, tipoBanho){
-
+return `Gato ${racaGato} fez um banho tipo: ${tipoBanho}`
 }
 
 export function tosaGato(racaGato, tipoTosa){
-    
+return `Gato ${racaGato} fez uma tosa tipo: ${tipoTosa}`  
 }
 
 export function alimentarGato(racaGato, tipoRacao){
-    
+return `Gato ${racaGato} fez uma alimentaçao tipo: ${tipoRacao}`  
 }
