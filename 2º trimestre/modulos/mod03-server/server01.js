489c8c233a27080320c4aefb49e706b9558ebd44
ThiagoKrudes <thiago.krudes.oliveira@escola.pr.gov.br>
2026-07-29 11:40:10 +0000

    criar servidor http com tratamento localhost

---
+++ 2º trimestre/modulos/mod03-server/server01.js
@@ -0,0 +1,20 @@
+const http = require('node:http')
+const porta = 8002
+
+const server = http.createServer((req, res)=>{
+    const novaUrl = new URL(req.URL,`http://${req.headers.host}`)
+    const caminhoUrl = novaUrl.pathname
+    if(caminhoUrl ==='/'){
+
+        res.statusCode = '201'
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('<h3>HTML Rocks!</h3>')
+    } else {
+        res.statusCode = '401'
+        res.setHeader('Content-Type', 'text/html; charset-utf-8')
+    }
+})
+server.listen(porta, ()=>{
+    console.log(`Servidor rodando na porta
+        http;//local""host:${porta}`)
+})
